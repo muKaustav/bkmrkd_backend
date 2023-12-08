43765eba6d621bceddb98e4872be8dab5edc14c1
muKaustav <mu.kaustav@gmail.com>
2023-12-08 05:59:18 +0530

    edit api

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const reviewRoutes = require("./routes/Review")
 const app = express()
 
 app.use(cors({
+    origin: 'http://192.168.1.5:3000',
     credentials: true,
 }))
 app.use(express.json())
